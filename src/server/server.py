d151b13803dd1e1f2576ad0b65a0dbfa52e0c5a4
K-Neg <35573703+K-Neg@users.noreply.github.com>
2020-11-15 14:39:47 -0300

    update requirements

--- src/server/server.py
+++ src/server/server.py
@@ -8,10 +8,8 @@ from server import routes
 app = FastAPI()
 app.include_router(routes.router)
 
-
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
 templates = Jinja2Templates(directory="src/templates")
-
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
