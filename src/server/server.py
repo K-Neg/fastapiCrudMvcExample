d707f6590275f2480490cfa029f41ac29cbb6883
K-Neg <35573703+K-Neg@users.noreply.github.com>
2020-11-15 14:38:10 -0300

    created folder structure

--- src/server/server.py
+++ src/server/server.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from server import routes
 
 app = FastAPI()
 app.include_router(routes.router)
 
-@app.get("/", tags=["Root"])
-async def read_root():
-    return {"message": "Welcome to this fantastic app!"}
+
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
+templates = Jinja2Templates(directory="src/templates")
 
 
-    
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("home.html",{"request": request})
