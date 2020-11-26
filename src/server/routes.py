17b44e46b61c12b5f53aa422e04bf6f1fedfb685
K-Neg <35573703+K-Neg@users.noreply.github.com>
2020-11-26 11:19:30 -0300

    Add Default CRUD Routes

--- src/server/routes.py
+++ src/server/routes.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import HTMLResponse
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 from src.database import (
     add_car,
@@ -32,14 +36,38 @@ html_content = """
 
 @router.get("/info")
 async def sendInfo():
-    return HTMLResponse(content=html_content, status_code=200)
+    return {
+        "CarSample":{
+            "owner": "Mauricio",
+            "manufacturer": "Ferrari",
+            "year": 2
+        }
+    }
 
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+class Carro(BaseModel):
+    owner: str
+    manufacturer: Optional[str] = None
+    year: Optional[int]
+    
+@router.post("/items/")
+async def create_item(item: Item):
+    return item
 
 # create
 @router.post("/post", response_description="A car has been created")
-async def add_car(car: CarSchema = Body(...)):
+#async def add_car_ROUTE(car: Carro):
+async def add_car_ROUTE(car: CarSchema = Body(...)):
+    print('pre Enc')
     car = jsonable_encoder(car)
+    print('pos enc')
     new_car = await add_car(car)
+    print("after new car")
     return ResponseModel(new_car, "Car created successfully.")
 
 
@@ -65,7 +93,8 @@ async def car_updater(id: str, req: UpdateCarModel = Body(...)):
     updated_car = await update_car(id, req)
     if updated_car:
         return ResponseModel(
-            "Car with register {} has bee updated".format(id),
+            "Car with register {} has been updated".format(id),
+            'this is the message ?'
         )
     return ErrorResponseModel("An error occurred", 404, ":(")
 
